abddc8e62c2d96ae6ddb2266524d529db3aaed5c
Maryam_Rahim <maryam.rahim@therightsw.com>
2024-10-07 15:49:26 +0500

    UI changes on home screen.

--- lib/lang/locale_keys.g.dart
+++ lib/lang/locale_keys.g.dart
@@ -3,76 +3,19 @@
 abstract class LocaleKeys {
   static const title = 'title';
   static const message = 'message';
-  static const confirmYourOrder = 'confirmYourOrder';
-  static const skipProductEntryOver = 'skipProductEntryOver';
-  static const loginToYourAccount = 'loginToYourAccount';
-  static const forgotYourPassword = 'forgotYourPassword';
   static const logIn = 'logIn';
-  static const orLogInWith = 'orLogInWith';
-  static const orderFirstCollection = 'orderFirstCollection';
-  static const newHere = 'newHere';
-  static const newCustomer = 'newCustomer';
-  static const submit = 'submit';
-  static const forgotPassword = 'forgotPassword';
-  static const yourInformation = 'yourInformation';
-  static const logInInformation = 'logInInformation';
-  static const agreeWithOur = 'agreeWithOur';
-  static const termsAndConditions = 'termsAndConditions';
-  static const tradingTerms = 'tradingTerms';
-  static const startOrder = 'startOrder';
-  static const orContinueWith = 'orContinueWith';
-  static const alreadyHaveAnAccount = 'alreadyHaveAnAccount';
-  static const search = 'search';
-  static const termsConditionPara1 = 'termsConditionPara1';
-  static const termsConditionPara2 = 'termsConditionPara2';
-  static const termsConditionPara3 = 'termsConditionPara3';
-  static const termsConditionPara4 = 'termsConditionPara4';
-  static const termsConditionPara5 = 'termsConditionPara5';
-  static const termsConditionPara6 = 'termsConditionPara6';
-  static const termsConditionPara7 = 'termsConditionPara7';
-  static const termsConditionPara8 = 'termsConditionPara8';
-  static const termsConditionPara9 = 'termsConditionPara9';
-  static const termsConditionPara10 = 'termsConditionPara10';
-  static const termsConditionPara11 = 'termsConditionPara11';
-  static const termsConditionPara12 = 'termsConditionPara12';
-  static const termsConditionPara13 = 'termsConditionPara13';
 
   /// ==================================== CHECKOUT DETAILS
   static const checkoutDetails = 'checkoutDetails';
   static const pickupAddress = 'pickupAddress';
   static const deliveryAddress = 'deliveryAddress';
   static const comments = 'comments';
-  static const commentsOptional = 'commentsOptional';
-  static const choosePickupAndDelivery = 'choosePickupAndDelivery';
-  static const paymentMethod = 'paymentMethod';
-  static const choosePaymentMethod = 'choosePaymentMethod';
-  static const details = 'details';
-  static const finalProductParagraph = 'finalProductParagraph';
-  static const minimumPurchase = 'minimumPurchase';
-  static const packagingFee = 'packagingFee';
-  static const countingService = 'countingService';
-  static const deliveryFee = 'deliveryFee';
-  static const pickupFee = 'pickupFee';
-  static const total = 'total';
-  static const addDiscountCode = 'addDiscountCode';
-  static const minimumPurchasePara1 = 'minimumPurchasePara1';
-  static const minimumPurchasePara2 = 'minimumPurchasePara2';
-  static const countingServicePara1 = 'countingServicePara1';
-  static const countingServicePara2 = 'countingServicePara2';
-  static const packagingFeePara1 = 'packagingFeePara1';
-  static const packagingFeePara2 = 'packagingFeePara2';
 
   /// ==================================== PICKUP ADDRESSES
 
   static const selectAddress = 'selectAddress';
   static const dateAndTime = 'dateAndTime';
   static const newAddress = 'newAddress';
-  static const selectDateAndTime = 'selectDateAndTime';
-  static const zipCode = 'zipCode';
-  static const proceed = 'proceed';
-  static const saveAddress = 'saveAddress';
-  static const back = 'back';
-  static const setPickupTime = 'setPickupTime';
 
   /// ==================================== PICKUP ADDRESSES
   static const setDeliveryTime = 'setDeliveryTime';
@@ -80,38 +23,21 @@ abstract class LocaleKeys {
   /// ==================================== COMMENTS
   static const save = 'save';
   static const commentsParaDryCleaner = 'commentsParaDryCleaner';
-  static const commentsParaDriver = 'commentsParaDriver';
-  static const toDryCleaner = 'toDryCleaner';
-  static const toDriver = 'toDriver';
-  static const saveThisCommentForFutureOrders =
-      'saveThisCommentForFutureOrders';
 
   /// ==================================== CREDIT CARD
 
   static const creditCard = 'creditCard';
   static const cardDetails = 'cardDetails';
   static const edit = 'edit';
-  static const nameOnCard = 'nameOnCard';
-  static const cardNumber = 'cardNumber';
-  static const expiryDate = 'expiryDate';
-  static const cvv = 'cvv';
 
   /// ====================================  ADD DISCOUNT
 
   static const thisCodeMayBeValidForNewUsersOnly =
       'thisCodeMayBeValidForNewUsersOnly';
-  static const add = 'add';
-  static const addDiscount = 'addDiscount';
-  static const orderPlaced = 'orderPlaced';
 
   /// ==================================== ORDER PLACED
 
   static const orderHistory = 'orderHistory';
-  static const backToHome = 'backToHome';
-  static const order = 'order';
-  static const date = 'date';
-  static const orderPlacedPara1 = 'orderPlacedPara1';
-  static const orderPlacedPara2 = 'orderPlacedPara2';
 
   /// ==================================== DRAWER
 
@@ -125,145 +51,13 @@ abstract class LocaleKeys {
 
   static const myOrders = 'myOrders';
   static const delivered = 'delivered';
-  static const onGoing = 'onGoing';
-  static const viewOrders = 'viewOrders';
-  static const downloadInvoice = 'downloadInvoice';
-  static const orderStatus = 'orderStatus';
 
   /// ==================================== MY PROFILE
 
   static const editProfile = 'editProfile';
   static const changePassword = 'changePassword';
   static const addresses = 'addresses';
-  static const billingAddress = 'billingAddress';
-  static const affiliateBusinessAgreement = 'affiliateBusinessAgreement';
-  static const linkOrUnsubscribeCreditCard = 'linkOrUnsubscribeCreditCard';
-  static const deleteAccount = 'deleteAccount';
-  static const saveChanges = 'saveChanges';
-  static const businessAgreementPara = 'businessAgreementPara';
-  static const companyCode = 'companyCode';
-  static const deleteAccountPara = 'deleteAccountPara';
-  static const yesDelete = 'yesDelete';
-  static const cancel = 'cancel';
-  static const logoutPara = 'logoutPara';
-  static const yesLogout = 'yesLogout';
 
-  static const invoiceEmailPara = 'invoiceEmailPara';
-  static const saveEmail = 'saveEmail';
-  static const invoiceEmail = 'invoiceEmail';
-  static const newStatus = 'newStatus';
-  static const pickedStatus = 'pickedStatus';
-  static const receivedStatus = 'receivedStatus';
-  static const readyStatus = 'readyStatus';
-  static const deliveredStatus = 'deliveredStatus';
-  static const myProfile = 'myProfile';
-  static const addNewAddress = 'addNewAddress';
-  static const editAddress = 'editAddress';
-  static const deleteAddress = 'deleteAddress';
-  static const makePrimary = 'makePrimary';
-  static const mustSelectPickupAddressFirst = 'mustSelectPickupAddressFirst';
-  static const pleaseSelectAddressFirst = 'pleaseSelectAddressFirst';
-  static const pleaseSelectAllAddresses = 'pleaseSelectAllAddresses';
-  static const editOrder = 'editOrder';
-  static const viewInvoice = 'viewInvoice';
-  static const youAreCurrentlyEditAnOrder = 'youAreCurrentlyEditAnOrder';
-  static const clickHere = 'clickHere';
-  static const areYouSureToDiscardTheOrderEdit =
-      'areYouSureToDiscardTheOrderEdit';
-  static const yes = 'yes';
-  static const no = 'no';
-  static const cancelEdit = 'cancelEdit';
-  static const email = 'email';
-  static const enterEmail = 'enterEmail';
-  static const firstName = 'firstName';
-  static const password = 'password';
-  static const lastName = 'lastName';
-  static const mobileNo = 'mobileNo';
-  static const address = 'address';
-  static const addCo = 'addCo';
-  static const addCreditCard = 'addCreditCard';
-  static const enterZipcode = 'enterZipcode';
-  static const enterAnyCo = 'enterAnyCo';
-  static const searchAddress = 'searchAddress';
-  static const noDeliveryIsAvailable = 'noDeliveryIsAvailable';
-  static const noPickupIsAvailable = 'noPickupIsAvailable';
-  static const Order = 'Order';
-  static const Date = 'Date';
-  static const enterEmailToGetInvoice = 'enterEmailToGetInvoice';
-  static const deleteOrder = 'deleteOrder';
-  static const phoneNumber = 'phoneNumber';
-  static const oldPassword = 'oldPassword';
-  static const newPassword = 'newPassword';
-  static const repeatNewPassword = 'repeatNewPassword';
-  static const enterOldPassword = 'enterOldPassword';
-  static const enterNewPassword = 'enterNewPassword';
-  static const addressTitle = 'addressTitle';
-  static const town = 'town';
-  static const defaultWord = 'defaultWord';
-  static const companyName = 'companyName';
-  static const cvr = 'cvr';
-  static const enterFirstName = 'enterFirstName';
-  static const enterLastName = 'enterLastName';
-  static const enterCompanyName = 'enterCompanyName';
-  static const enterCVR = 'enterCVR';
-  static const enterZipCode = 'enterZipCode';
-  static const code = 'code';
-  static const enterPassword = 'enterPassword';
-  static const enterAnyCoOptional = 'enterAnyCoOptional';
-  static const enterPostcodeToContinue = 'EnterPostcodeToContinue';
-  static const noAddressFound = 'noAddressFound';
-  static const placeOrder = 'placeOrder';
-  static const somethingWentWrong = 'somethingWentWrong';
-  static const continueWord = 'continueWord';
-  static const emailAddressIsRequired = 'emailAddressIsRequired';
-  static const enterValidEmail = 'enterValidEmail';
-  static const mobileNumberIsRequired = 'mobileNumberIsRequired';
-  static const pleaseEnterAValid8digitPhoneNumber =
-      'pleaseEnterAValid8digitPhoneNumber';
-  static const enterAValidFirstNameOfAtLeast3Characters =
-      'enterAValidFirstNameOfAtLeast3Characters';
-  static const enterAValidLastNameOfAtLeast3Characters =
-      'enterAValidLastNameOfAtLeast3Characters';
-  static const enterAddress = 'enterAddress';
-  static const enterCompany = 'enterCompany';
-
-  static const passwordIsRequired = 'passwordIsRequired';
-  static const passwordMustBeAtLeast5CharactersLong =
-      'passwordMustBeAtLeast5CharactersLong';
-  static const zipCodeIsRequired = 'zipCodeIsRequired';
-  static const enterValidCode = 'enterValidCode';
-
-  static const enterAValidCompanyOfAtLeastCharacters =
-      'enterAValidCompanyOfAtLeastCharacters';
-  static const enterAValidAddressOfAtLeastCharacters =
-      'enterAValidCompanyOfAtLeastCharacters';
-  static const discount = 'discount';
-  static const noOrder = 'noOrder';
-  static const tryAgain = 'tryAgain';
-  static const selectCompleteAddress = 'selectCompleteAddress';
-  static const cvrIsRequired = 'cvrIsRequired';
-  static const coRequired = 'coRequired';
-  static const companyNameIsRequired = 'companyNameIsRequired';
-  static const readyToMakeYourFirstOrderTapHerePara1 =
-      "readyToMakeYourFirstOrderTapHerePara1";
-  static const readyToMakeYourFirstOrderTapHerePara2 =
-      "readyToMakeYourFirstOrderTapHerePara2";
-  static const welcomeBack = "welcomeBack";
-  static const version = "version";
-  static const cusId = "cusId";
-  static const daysLongerProcessingTime = "daysLongerProcessingTime";
-  static const dayLongerProcessingTime = "dayLongerProcessingTime";
-  static const enterYourCommentHere = "enterYourCommentHere";
-  static const comment = "comment";
-  static const noProducts = "noProducts";
-  static const enterZipCodeToContinue = "enterZipCodeToContinue";
-  static const noAddressSelected = "noAddressSelected";
-  static const pleaseAgreeTheTermsAndConditionsToContinue =
-      "pleaseAgreeTheTermsAndConditionsToContinue";
-  static const sessionExpired = "sessionExpired";
-  static const connectionError = "connectionError";
-  static const unknownError = "unknownError";
-  static const noInternetError = "noInternetError";
   static const badCertificateError = "badCertificateError";
   static const enterCode = "enterCode";
   static const codeIsRequired = "codeIsRequired";
